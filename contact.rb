dae2880f80c8ce4a691c5bfc8fdb5d69d2fdb472
JurgenHahn <jurgenehahn@gmail.com>
2017-01-24 13:01:53 -0500

    first commit

--- contact.rb
+++ contact.rb
@@ -1,6 +1,79 @@
-# Copy your entire Contact class from the old crm assignment here.
-# Then, run the following at the command line:
-#
-#   ruby test/contact_test.rb
-#
-# If your implementation of the Contact class is 'right', then you should see that all tests pass!
+class Contact
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
+
+  @@contacts = []
+  @@next_id = 1
+
+  def initialize(first_name, last_name, email, note)
+    @id = @@next_id
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+
+    @@next_id += 1
+  end
+
+
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
+
+  end
+
+  def self.all
+    @@contacts
+  end
+
+  def self.find(by_id)
+    @@contacts.each do |contact|
+      if contact.id == by_id
+        return contact
+      end
+    end
+
+    contact = nil
+  end
+
+
+  def update(attribute, new_value)
+    #self.send("#{attribute}=", new_value)
+    if attribute == "email"
+      self.email = new_value
+    elsif attribute == "first_name"
+      self.first_name = new_value
+    elsif attribute == "last_name"
+      self.last_name = new_value
+    elsif attribute == "note"
+      self.note = new_value
+     end
+  end
+
+
+  def self.find_by(attribute, value)
+    @@contacts.each do |contact|
+      if contact.send(attribute) == value
+        return contact
+      end
+    end
+    nil
+  end
+
+
+  def self.delete_all
+    @@contacts = []
+    #empties array but does not delete contacts
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+def delete
+  @@contacts.delete(self)
+end
+
+
+end
